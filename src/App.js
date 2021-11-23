62054edb522b703ec85c492a0d02525b46e3ea7c
Abrorwebdeveloper <bogibekovabror5696@gmail.com>
2021-11-23 21:23:28 +0500

    add increase and decrease function to Appp.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <h1>Hello React</h1>
-  );
+  const [count, setCount] = useState( 0 )
+
+  function Inc() {
+    setCount(prev => prev + 1)
+  } 
+
+  function Dec() {
+    setCount(prev => prev - 1)
+  }
+  return(
+    <>
+      <h3>Count: { count } </h3>
+      <button onClick = { Inc } className = "btn1">Inc</button>
+      <button onClick = { Dec } className = "btn2">Dec</button>
+  </>
+  )
+   
 }
 
 export default App;
